073af85fc3bba8427a2f6367ba887ae242fd08c9
e.androschuk <e.androschuk@qiwi.com>
2019-09-23 18:57:34 +0300

    Added button create manufacture

--- src/main/java/motolibrary/controller/MainController.java
+++ src/main/java/motolibrary/controller/MainController.java
@@ -1,11 +1,13 @@
 package motolibrary.controller;
 
+import motolibrary.model.Manufacture;
 import motolibrary.service.dao.made.MadeDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -22,6 +24,24 @@ public class MainController {
     public String index(Model model) {
         model.addAttribute("manufactures", madeDao.getAllManufactures());
         return "index";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "manufacture/create")
+    public String createMake(
+        ) {
+        return "manufacture/create_make";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "manufacture/create/response")
+    public String createMakeResponse(
+        Model model,
+        @RequestParam String description,
+        @RequestParam String country
+    ) {
+        Manufacture manufacture = new Manufacture(null, null, description, country);
+        model.addAttribute("manufacture", manufacture);
+        madeDao.createModel(manufacture);
+        return "manufacture/create_make_response";
     }
 
 }
